ba485c308bfe760e41d0b881ae9f8a16fb7f67f2
Geoffrey Bonnanzio <gbonnanzio@gmail.com>
2020-06-08 22:24:11 -0400

    rearranged sooo many things and redefined the functions

--- potentialEnergy.py
+++ potentialEnergy.py
@@ -4,73 +4,74 @@ import numpy as np
 import math
 import os
 
-def determinePE(coordFile):
+def determinePE(xPos,yPos,zPos):
     
-    #coordFile is a txt file of the coords at a single time step
-    data = np.loadtxt(fname = coordFile)
-
-    xPos = data[:,2]
-    yPos = data[:,3]
-    zPos = data[:,4]
-
-    nAtoms = data.shape[0]
-    totalPotential = 0
+    nAtoms = len(xData)
+    allR = []
+    allPE = []
+    totalPE = 0
+    totalR = 0
 
     for i in range(nAtoms):
         for j in range(i+1,nAtoms):
-            if i != j:
-                #only execute if two different atoms
-                #positions
-                x1 = xPos[i]
-                x2 = xPos[j]
-                y1 = yPos[i]
-                y2 = yPos[j]
-                z1 = zPos[i]
-                z2 = zPos[j]
-     
-                #difference in pos
-                diffX = x2-x1
-                diffY = y2-y1
-                diffZ = z2-z1
+            #only execute if two different atoms
+            #positions
+            x1 = xPos[i]
+            x2 = xPos[j]
+            y1 = yPos[i]
+            y2 = yPos[j]
+            z1 = zPos[i]
+            z2 = zPos[j]
+ 
+            #difference in pos
+            diffX = x2-x1
+            diffY = y2-y1
+            diffZ = z2-z1
 
-                #find r
-                r = math.sqrt((diffX**2)+(diffY**2)+(diffZ**2))
+            #find r
+            r = math.sqrt((diffX**2)+(diffY**2)+(diffZ**2))
+            totalR = totalR + r
 
-                #constants
-                Ar_diam = 142.0 #nm
-                #from White 1999
-                eps = 125.0*(8.6173*10**-5) #eV
-                sigma = 0.3345 #nm
+            #constants
+            Ar_diam = 142.0 #nm
+            #from White 1999
+            eps = 1.0 #125.0*(8.6173*10**-5) #eV
+            sigma = 0.3345 #nm
 
-                rCorrected = r*sigma
-                sigOverR = sigma/rCorrected
-                tempPot = 4.0*eps*((sigOverR**12)-(sigOverR**6))
+            rCorrected = r*sigma
+            sigOverR = sigma/rCorrected
+            tempPE = 4.0*eps*((sigOverR**12)-(sigOverR**6))
+            
+            #add to total potential
+            totalPE = totalPE + tempPE
+        
+        avgPE = totalPE/(nAtoms-1)
+        allPE.append(avgPE)
+        avgR = totalR/(nAtoms-1)
+        allR.append(avgR)
 
-                #add to total potential
-                totalPotential = totalPotential + tempPot
-
-
-    return totalPotential
+    return allPE,allR
     
 
 def main():
     #specify where the text file is located
     filePath = open('/Users/gbonn/Summer_Research_2020/lammps_tut/allCoords.txt',r)
-    xPos = []
-    yPos = []
-    zPos = []
+    xData = []
+    yData = []
+    zData = []
     
     timeStep = 0
     for lineNum, line in enumerate(filePath):
         testLine = lineNum%502
-        if (testLine == 0):
-            timeStep = timeStep + 1
-        elif(testLine != 1):
+        while(testLine != 0 and testLine!=1)):
             lineList = line.split()
-            xPos.append(lineList[1])
-            yPos.append(lineList[2])
-            zPos.append(lineList[3])
+            xData.append(float(lineList[1]))
+            yData.append(float(lineList[2]))
+            zData.append(float(lineList[3]))
+        if(xData != ""):
+            determinePE(xData,yData,zData)
 
+            
 
 
     
