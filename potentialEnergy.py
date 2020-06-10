9cf70733ff246752e8ebbd0c657f0036a784f52a
Geoffrey Bonnanzio <gbonnanzio@gmail.com>
2020-06-10 10:39:25 -0400

    first attempt at binning

--- potentialEnergy.py
+++ potentialEnergy.py
@@ -46,24 +46,41 @@ def determinePE(xPos,yPos,zPos):
     allR = np.sqrt(allR)
     return allR, allPE
     
-def minMaxRVal(rValues):
-    #function that tells me min and max r over all data so 
-    #that I can create appropriate bin sizes
+def binning(allRValues,allPEValues,numBins):
+    numBins = float(numBins)
+    binSpace = math.ceil((math.sqrt(10.0*math.sqrt(3))/(numBins)))
+    rPEPairs = [[]]
+    currR = 0
+    for indx in range(numBins):
+        rPEPairs[indx][0] = currR
+        currR = currR + binSpace
+    for timeStep in range(len(allRValues)):
+        for atomRIndx in range(len(allRValues[timeStep])):
+            tempR = allRValues[timeStep][atomRIndx]
+            binIndx = -1
+            while (tempR < rPEPairs[binIndx]):
+                binIndx = binIndx - 1
+            tempPE = allPEValues[timeStep][atomRIndx]
+            if(len(rPEPairs[binIndx]) == 1):
+                rPEPairs[binIndx][1] = tempPE
+                rPEPairs[binIndx][2] = 1.0
+            else:
+                totPrevPE = rPEPairs[binIndx][1]*rPEPairs[binIndx][2]
+                rPEPairs[binIndx][2] = rPEPairs[binIndx][2] + 1.0
+                rPEPairs[binIndx][1] = (totPrevPE + tempPE)/rPEPairs[binIndx][2]
 
-    tempMin = min(rValues)
-    tempMax = max(rValues)
-    return tempMin, tempMax
+
+    return rPEPairs
 
 def main():
     #specify where the text file is located
-    filePath = open('/Users/gbonn/Summer_Research_2020/lammps_tut/lastTimeStep.txt','r')
+    filePath = open('/Users/gbonn/Summer_Research_2020/lammps_tut/melt.lmpdump','r')
+    #outFile = open('/Users/gbonn/Summer_Research_2020/lammps_tut/rAndPE_pairs.txt','w')
     xData = []
     yData = []
     zData = []
     allMinR = []
     allMaxR = []
-    currMin = 1000
-    currMax = 0
     allR = []
     allPE = []
     
@@ -75,34 +92,33 @@ def main():
             yData.append(float(lineList[3]))
             zData.append(float(lineList[4]))
             if(testLine == 508):
+                #print(xData[-1],yData[-1],zData[-1])
                 tempR,tempPE = determinePE(xData,yData,zData)
                 allR.append(tempR)
                 allPE.append(tempPE)
-                #if(tempMin<currMin):
-                 #   currMin = tempMin
-                #if(tempMax>currMax):
-                 #   currMax = tempMax
-                if(lineNum > 510):
+                xData = []
+                yData = []
+                zData = []
+                #if(lineNum > 508):
+                    #outFile.write(str(tempR) + " " + str(tempPE) + "\n")
+                if(lineNum >= 500):
                     break
     
+    finalPairs = binning(allR,allPE,10)
+    print(finalPairs)
     #print(currMin,currMax)
     filePath.close()
-
-    #outFile = open('/Users/gbonn/Summer_Research_2020/lammps_tut/testOutput.txt','w')
-    #for indx in range(len(finalR)):
-    #    outFile.write(str(finalR[indx]) + ' PE:')
-    #    outFile.write(str(finalPE[indx])+'\n')
     #outFile.close()
     
     #plot potential energy at each time step
-    print(len(allR[0]),len(allPE[0]))
-    plt.plot(allR[0],allPE[0],'ro',ms = 1)
-    plt.xlim(0,10)
-    plt.ylim(-2,15)
+    #print(len(allR[0]),len(allPE[0]))
+    #plt.plot(allR[0],allPE[0],'ro',ms = 1)
+    #plt.xlim(0,10)
+    #plt.ylim(-2,15)
     #plt.plot(allR[1],allPE[1],'ro')
-    plt.xlabel('Distance')
-    plt.ylabel('Potential Energy')
-    plt.show()
+    #plt.xlabel('Distance')
+    #plt.ylabel('Potential Energy')
+    #plt.show()
 
 #run functions    
 main()
